2789c6d44a7321aa19661905655bfad40fc4f225
Vaidik  Chandravanshi <126126126.vaidik@gmail.com>
2025-04-24 16:36:21 +0530

    Update App.jsx
    A

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,16 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Components/Home';
+const App = () => {
+  return (
+    <div className='h-screen w-screen flex'>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+        </Routes>
+      </Router>
+    </div>
+  )
+}
 
+export default App;

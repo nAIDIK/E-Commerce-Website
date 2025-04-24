ae4b5a30a8d3797cdea2b9f3365aa1c58e156397
Vaidik  Chandravanshi <126126126.vaidik@gmail.com>
2025-04-24 16:27:50 +0530

    Update Nav.jsx
    C

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import Nav from "./Nav";
 
-const Nav = () => {
+const Home = () => {
   return (
-    <div>Nav</div>
-  )
-}
+    <>
+      <Nav />
+      <div className="container h-full w-[85%] flex flex-wrap overflow-x-hidden overflow-y-auto p-10 pt-[5%]">
+        <div className="card w-[15%] h-[30vh] flex p-3 border shadow mr-3 mb-3 flex-col justify-center items-center">
+          <div
+            className="image w-full h-[80%] bg-contain bg-no-repeat bg-center mb-3 hover:scale-110"
+            style={{
+              backgroundImage:
+                "url(https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg)",
+            }}
+          ></div>
+          <h1 className="hover:text-blue-300">Lorem ipsum dolor</h1>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Nav;
+export default Home;

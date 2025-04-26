a7db4827ee285c87f043c0dd3ae3bab887466717
nAIDIK <you@example.com>
2025-04-26 16:43:00 +0530

    Details

---
+++ src/Components/Details.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Details = () => {
+  return (
+    <div className=' bg-red-300 w-[70%] min-h-screen m-auto p-[10%] flex items-center justify-between'>
+          
+        </div>
+  )
+}
+
+export default Details

f797316291c17f13a0b66d01562642515a542fe5
nAIDIK <you@example.com>
2025-05-07 16:37:30 +0530

    nav

--- src/Components/Create.jsx
+++ src/Components/Create.jsx
@@ -1,44 +1,101 @@
-import React from "react";
+import React, { useState } from "react";
+
 
 const Create = () => {
+  const [ image, setImage] = useState("");
+  const [title, setTitle] = useState("");
+  const [category, setCategory] = useState("");
+  const [price, setPrice] = useState("");
+  const [description, setDescription] = useState("");
+
+const submitHandler =(e) => {
+e.preventDefault();
+const newProduct ={
+  image,
+  title,
+  category,
+  price,
+  description,
+}
+console.log(newProduct);
+};
+
+
+const changeImage =(e) => {
+  setImage(e.target.value);
+  console.log(image);
+};
+
+const changeTitle =(e) => {
+  setTitle(e.target.value);
+  console.log(title);
+};
+
+const changeCategory =(e) => {
+  setCategory(e.target.value);
+  console.log(category);
+};
+
+const changePrice =(e) => {
+  setPrice(e.target.value);
+  console.log(price);
+};
+
+const changeDescription =(e) => {
+  setDescription(e.target.value);
+  console.log(description);
+};
+
   return (
     <>
-      <form className="w-full h-full flex flex-col items-center justify-center gap-5  ">
+      <form
+      onSubmit={submitHandler}
+       className="w-full h-full flex flex-col items-center justify-center gap-5  ">
         <h1 className="text-4xl">Add New Product</h1>
         <input
           className="h-10 w-1/2  p-2 border  rounded"
           type="URL"
           placeholder="Enter your image url here"
           required
+          onChange={changeImage}
         />
+
         <input
           className="h-10 w-1/2  p-2 border  rounded"
           type="text"
           placeholder="Enter your title  "
           required
+          onChange={changeTitle}
         />
+
         <input
           className="h-10 w-1/2  p-2 border  rounded"
           type="text"
           placeholder="Enter your category "
           required
+          onChange={changeCategory}
         />
+
         <input
           className="h-10 w-1/2  p-2 border  rounded"
           type="number"
           placeholder="Enter your price"
           required
+          onChange={changePrice}
         />
+
         <textarea
           className="h-10 w-1/2  p-2 border  rounded"
           placeholder="Enter your description"
           required
+          onChange={changeDescription}
+
         ></textarea>
         <button
           className="py-2 px-5  rounded -blue-300 border border-blue-300 text-blue-400"
           to="/create"
         >
-          Add new product
+          Submit
         </button>
       </form>
     </>
--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import{ProductContext} from "../Utils/Context"
 
 const Nav = () => {
+
+const [products] = useContext (ProductContext);
+
+
+let category =
+products && products.reduce((acc, cv) =>[...acc, cv.category] ,[]);
+category = [...new Set(
+  category
+)];
+console.log(category);
+
+
+
   return (
     <nav className="w-[15%] h-full bg-zinc-100 pt-5 flex items-center flex-col">
       <Link
@@ -11,24 +26,18 @@ const Nav = () => {
         Add New Product
       </Link>
       <hr className="w-[80%] my-3" />
-      <h1 className="text-2xl">Category Filter</h1>
-      <ul className="w-[80%] mt-2">
-        <li className="mb-3 flex items-center">
-          {" "}
-          <span className="w-[15px] h-[15px] bg-blue-200 rounded-full mr-2"></span>
-          Male
-        </li>
-        <li className="mb-3 flex items-center">
-          {" "}
-          <span className="w-[15px] h-[15px] bg-red-200 rounded-full mr-2"></span>
-          Female
-        </li>
-        <li className="mb-3 flex items-center">
-          {" "}
-          <span className="w-[15px] h-[15px] bg-green-200 rounded-full mr-2"></span>
-          Kids
-        </li>
-      </ul>
+      <h1 className="text-2xl text-cyan-300 "> Category Filter</h1>
+
+      <div className="w-[80%] mt-2">
+      {category.map((cat  ) => (
+        <Link 
+        className="mb-3 flex items-center">
+        <span className="w-[15px] h-[15px] bg-blue-200 rounded-full mr-2"></span>
+        {cat}
+      </Link>
+      ))}
+        
+      </div>
     </nav>
   );
 };
